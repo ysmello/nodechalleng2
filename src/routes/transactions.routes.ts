e9f089655542e6858b8b29707a1aa51f6823b70c
Yuri Mello <yuridemeello@gmail.com>
2020-08-03 07:35:03 -0300

    Rota para criar uma transação

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,18 +1,22 @@
 import { Router } from 'express';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
-transactionsRouter.get('/', async (request, response) => {
-  // TODO
-});
+transactionsRouter.get('/', async (request, response) => {});
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type /* category */ } = request.body;
+
+  const transactionService = new CreateTransactionService();
+
+  const transaction = await transactionService.execute({ title, value, type });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,27 @@
+import { getRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, value, type }: Request): Promise<Transaction> {
+    const transactionRepository = getRepository(Transaction);
+
+    const transaction = transactionRepository.create({ title, value, type });
+
+    await transactionRepository.save({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
